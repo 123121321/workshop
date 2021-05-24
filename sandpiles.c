a0f9490ba484978fb26cec6cb550d8e545fd1e69
peggyjay <spengrob@gmail.com>
2021-05-24 16:57:03 -0400

    fixed code not toppling correctly which i somehow missed removed colors as the color pallette wasnt too pleasing. redid spacing throughout the file to make code easier to read TODO: accept user input so as to make creating grids less painful see if i can reduce the topple function to 4 cases for each possible neighbor. ditch console printing for grids and instead output to an image file (plan for sandpiles2.c)

--- sandpiles.c
+++ sandpiles.c
@@ -2,35 +2,25 @@
 #include <stdlib.h>
 
 #define HOLDING_CAPACITY 4
-#define GRID_X 7
-#define GRID_Y GRID_X
-// define GRID_Y with something else for non-square grids
+#define GRID_HEIGHT 9
+#define GRID_WIDTH GRID_HEIGHT
+// define GRID_WIDTH with something else for non-square grids
 // using odd values to ensure that there is a single "middle"
 // for the sake of symmetry
 
-const char* colors[7] = {
-    "\x1b[31m",
-    "\x1b[32m",
-    "\x1b[33m",
-    "\x1b[34m",
-    "\x1b[35m",
-    "\x1b[36m",
-    "\x1b[37m"
-};
-
-int grid[GRID_X][GRID_Y];
+int grid[GRID_HEIGHT][GRID_WIDTH];
 
 /* the following function is a little opaque in why ints are
  being returned to determine the type of a cell.
  picture an XxY grid, the returns should be fairly self explanatory
 
-		0      6      3
-		 0,0  ...  X,0
-		  .         .
-		2 .    8    . 5
-		  .         .
-		 0,Y  ...  X,Y
-		1      7      4
+    0      2      1
+     0,0  ...  0,Y
+      .         .
+    6 .    8    . 7
+      .         .
+     X,0  ...  X,Y
+    3      5      4
 
  is there a better way to assign cells a type and
  direction/neighbors? almost certainly, but thats for the 
@@ -40,22 +30,22 @@ int cornerEdgeOrBody(int x, int y) {
     if (x == 0) {
       if (y == 0) {
             return 0;
-        } else if (y == (GRID_Y-1)) {
+        } else if (y == (GRID_WIDTH-1)) {
             return 1;
         } else {
             return 2;
         }
-    } else if (x == (GRID_X-1)) {
+    } else if (x == (GRID_HEIGHT-1)) {
         if (y == 0) {
             return 3;
-        } else if (y == (GRID_Y-1)) {
+        } else if (y == (GRID_WIDTH-1)) {
             return 4;
         } else {
             return 5;
         }
     } else if (y == 0) {
         return 6;
-    } else if (y == (GRID_Y-1)) {
+    } else if (y == (GRID_WIDTH-1)) {
         return 7;
     } else {
         return 8;
@@ -69,48 +59,66 @@ int cornerEdgeOrBody(int x, int y) {
 // be required as grids get larger
 int topple(int *value, int x, int y) {
     if (*value >= HOLDING_CAPACITY) {
-			int cellType = cornerEdgeOrBody(x, y);
-			*value -= 4;
-			switch(cellType) {
-				case 0:
-					++*(value + 1);
-					++*(value + GRID_X);
-				case 1:
-					++*(value + 1);
-					++*(value - GRID_X);
-				case 2:
-					++*(value + 1);
-				case 3:
-					++*(value - 1);
-					++*(value + GRID_X);
-				case 4:
-					++*(value - 1);
-					++*(value - GRID_X);
-				case 5:
-					++*(value - 1);
-				case 6:
-					++*(value + GRID_X);
-				case 7:
-					++*(value - GRID_X);
-				case 8:
-					++*(value - 1);
-					++*(value + 1);
-					++*(value - GRID_X);
-					++*(value + GRID_X);
-			}
-			return 1;
+      int cellType = cornerEdgeOrBody(x, y);
+      int cellValue = *value;
+      int spillover = cellValue/4;
+      *value -= (4 * spillover);
+      switch(cellType) {
+        case 0:
+          *(value + 1) += spillover;
+          *(value + GRID_WIDTH) += spillover;
+          break;
+        case 1:
+          *(value - 1) += spillover;
+          *(value + GRID_WIDTH) += spillover;
+          break;
+        case 2:
+          *(value + 1) += spillover;
+          *(value - 1) += spillover;
+          *(value + GRID_WIDTH) += spillover;
+          break;
+        case 3:
+          *(value + 1);
+          *(value - GRID_WIDTH) += spillover;
+          break;
+        case 4:
+          *(value - 1) += spillover;
+          *(value - GRID_WIDTH) += spillover;
+          break;
+        case 5:
+          *(value - 1) += spillover;
+          *(value + 1) += spillover;
+          *(value - GRID_WIDTH) += spillover;
+          break;
+        case 6:
+          *(value + GRID_WIDTH) += spillover;
+          *(value + 1) += spillover;
+          *(value - GRID_WIDTH) += spillover;
+          break;
+        case 7:
+          *(value - GRID_WIDTH) += spillover;
+          *(value + GRID_WIDTH) += spillover;
+          *(value - 1) += spillover;
+          break;
+        case 8:
+          *(value - 1) += spillover;
+          *(value + 1) += spillover;
+          *(value - GRID_WIDTH) += spillover;
+          *(value + GRID_WIDTH) += spillover;
+      }
+      return 0;
     } else {
-			return 0;
-		}
+      return 1;
+    }
 };
 
 void printGrid() {
-	for (int i = 0; i < GRID_X;)
+  for (int i = 0; i < GRID_HEIGHT;)
   {
-      for (int j = 0; j < GRID_Y;)
+      for (int j = 0; j < GRID_WIDTH;)
       {
-          int *cellPointer = &grid[i][j];
-          printf("%s%d", colors[*cellPointer], *cellPointer);
+          int *pCell = &grid[i][j];
+          printf("%d  ", *pCell);
           ++j;
       }
       printf("\n");
@@ -118,42 +126,44 @@ void printGrid() {
   }
 };
 
+void printCell() {
+  for (int i = 0; i < GRID_HEIGHT;)
+  {
+    for (int j = 0; j < GRID_WIDTH;)
+    {
+      int *pCell = &grid[i][j];
+      int cellType = cornerEdgeOrBody(i, j);
+      printf("cell at %d,%d is %d, value: %d\n", i, j, cellType, *pCell);
+      ++j;
+    }
+    ++i;
+  }
+};
+
 int main() {
-    grid[1][1] = 6;
-		grid[2][1] = 3;
-		grid[1][2] = 3;
-		grid[3][2] = 5;
-		grid[2][5] = 2;
-		grid[1][3] = 3;
-		grid[2][3] = 1;
-		grid[6][6] = 3;
-		grid[6][5] = 3;
-		grid[6][4] = 3;
-		grid[5][6] = 3;
-		grid[5][5] = 4;
-		grid[5][4] = 3;
-		grid[4][6] = 3;
-		grid[4][5] = 3;
-		grid[4][4] = 3;
-    printf("INITIAL GRID CONFIGURATION\n");
-		printGrid();
-		int toppleCheck = 1;
-		while (toppleCheck == 1) {
-			printf("\x1b[0mTOPPLING. . .\n");
-			for (int i = 0; i < GRID_X;)
-			{
-				for (int j = 0; j < GRID_Y;)
-				{
-					int *cellPointer = &grid[i][j];
-					toppleCheck = topple(cellPointer, i, j);
-					++j;
-				}
-				++i;
-			};
-			printGrid();
-		};
-    printf("\x1b[0mGRID AFTER TOPPLING\n");
-		printGrid();
-    puts("\x1b[0m");
-    return 0;
+  grid[4][4]=124;
+  printf("INITIAL GRID CONFIGURATION\n");
+  printGrid();
+  int toppleCheck = 0;
+  while (toppleCheck < (GRID_WIDTH * GRID_HEIGHT)) {
+    toppleCheck = 0;
+    printf("\x1b[0mTOPPLING. . .\n");
+    for (int i = 0; i < GRID_HEIGHT;)
+    {
+      for (int j = 0; j < GRID_WIDTH;)
+      {
+        int *pCell = &grid[i][j];
+        toppleCheck += topple(pCell, i, j);
+        ++j;
+      }
+      ++i;
+
+    };
+    printGrid();
+    if (toppleCheck == (GRID_WIDTH * GRID_HEIGHT)) {
+      break;
+    }
+  };
+  puts("\x1b[0m");
+  return 0;
 };
